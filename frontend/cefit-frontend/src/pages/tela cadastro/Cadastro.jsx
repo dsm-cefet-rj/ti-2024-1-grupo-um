827871db9bc71d256a188222ce93cedd6a983fc1
Caio Santos <caiosantosdev@gmail.com>
2024-03-29 14:32:40 -0300

    fixed cadastro and login pages

--- frontend/cefit-frontend/src/pages/tela cadastro/Cadastro.jsx
+++ frontend/cefit-frontend/src/pages/tela cadastro/Cadastro.jsx
@@ -6,13 +6,29 @@ import "./../pages.css";
 
 function Cadastro(){
     return(
-        <div>
-            <Navbar/>
-            <div className="bg-image cefit-background-img"style=
-                {{backgroundImage: `url('https://media.licdn.com/dms/image/C4D1BAQHmSj8U8aUKGw/company-background_10000/0/1583773755115/grupo_wave_cover?e=2147483647&v=beta&t=xa3nLX_MondQhxOWso-6bVlkLaBGgteKc897UK609TI')`,
-                height: "100vh",
-                width: "100vw"}}>
-                <div className="div-principal container d-flex align-items-center justify-content-center m-auto">
+            <div class="bg-image cefit-background-img" style={{backgroundImage: `url('https://img.goodfon.com/original/1920x1080/e/97/shtanga-skamya-gym-fitness.jpg')`}}>
+                <div class="login-container rounded-5 p-3">
+                    <CefetImage/>
+                    <form class="formulario-cadastro">
+                        <InputComponent classes="mt-3" id="nameImput" text="Nome Completo" type="text" placeholder="Seu nome completo aqui"/>
+                        <InputComponent classes="" id="InputEmail" text="Email" type="email" placeholder="Insira seu email aqui"/>
+                        <InputComponent classes="" id="CPFInput" text="CPF" type="text" placeholder="Seu CPF aqui"/>
+                        <InputComponent classes="" id="age" text="Data de Nascimento" type="date" placeholder=""/>
+                        <InputComponent classes="" id="Password" text="Senha" type="password" placeholder="Insira sua senha aqui"/>
+                        <SubmitButton nomeButton="Cadastrar"/>
+                        <div class="cadastro-texto mt-3">
+                            Possui conta?<a href="./../tela-login/Login.jsx"> Faça o seu Login!</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    );
+}
+{/* <div className="bg-image cefit-background-img"style=
+                {{backgroundImage: `url('https://img.goodfon.com/original/1920x1080/e/97/shtanga-skamya-gym-fitness.jpg')`,
+                height: "100%",
+                width: "100%"}}>
+                <div className="div-principal d-flex align-items-center justify-content-center mb-4   ">
                     <div className="w-50 rounded-5 p-4 login-container">
                         <CefetImage/>
                         <form className="formulario-cadastro">
@@ -28,9 +44,5 @@ function Cadastro(){
                         </form>
                     </div>
                 </div>
-            </div>
-        </div>
-    );
-}
-
+            </div> */}
 export default Cadastro;
