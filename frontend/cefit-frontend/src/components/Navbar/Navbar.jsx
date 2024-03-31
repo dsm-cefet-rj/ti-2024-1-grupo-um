a384ee0a91f810832cc596a929b13082a44832ea
unknown <caiosantosdev@gmail.com>
2024-03-31 12:25:41 -0300

    trying navbar test

--- frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
+++ frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
@@ -4,39 +4,38 @@ import logo from "./logo.png"
 function Navbar(){
     return(
         <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container-fluid">
+            <div class="container-fluid">
+                {/* logo */}
+                <a class="navbar-brand me-auto" href="#"><img src={logo} alt="logo" class="logo-img"/></a>
 
-            <a class="navbar-brand me-auto" href="#"><img src={logo} alt="logo" class="logo-img"/></a>
-
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" class="logo-img"/></h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" class="logo-img"/></h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                            <li class="nav-item">
+                                <a class="nav-link mx-lg-2" aria-current="page" href="/frontend/prototipo-html/index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link mx-lg-2 active" href="/frontend/prototipo-html/fit-area/fit.html">Área FIT</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link mx-lg-2" href="/frontend/prototipo-html/trainers-area/page2.html">Personais</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" aria-current="page" href="/frontend/prototipo-html/index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2 active" href="/frontend/prototipo-html/fit-area/fit.html">Área FIT</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="/frontend/prototipo-html/trainers-area/page2.html">Personais</a>
-                        </li>
-                    </ul>
+                <div class="d-flex">
+                    <a href="./../login-area/login.html" class="signin-button">Sign in</a>
+                    <a href="./../sign-up-area/cadastro.html" class="signup-button">Sign up</a>
                 </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
             </div>
-            <div class="d-flex">
-                <a href="./../login-area/login.html" class="signin-button">Sign in</a>
-                <a href="./../sign-up-area/cadastro.html" class="signup-button">Sign up</a>
-            </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-        </div>
-    </nav>
+        </nav>
     )
 }
 
--- frontend/cefit-frontend/src/pages/tela cadastro/Cadastro.jsx
+++ frontend/cefit-frontend/src/pages/tela cadastro/Cadastro.jsx
@@ -1,6 +1,7 @@
 import InputComponent from "../../components/InputComponent/InputComponent";
 import CefetImage from "../../components/CefetImage/CefetImage";
 import SubmitButton from "../../components/SubmitButton/SubmitButton";
+// import Navbar from "../../components/Navbar/Navbar";
 import { useState } from "react";
 import "./../pages.css";
 // import { createHash } from "crypto";
@@ -30,6 +31,7 @@ function Cadastro(){
 
     return(
         <div className="bg-image cefit-background-img" style={{backgroundImage: `url('https://img.goodfon.com/original/1920x1080/e/97/shtanga-skamya-gym-fitness.jpg')`}}>
+            {/* <Navbar/> */}
                 <div className="login-container rounded-5 p-3">
                     <CefetImage/>
                     <form className="formulario-cadastro" onSubmit={handleSingUp}>
@@ -47,47 +49,4 @@ function Cadastro(){
             </div>
     );
 }
-{/* <div className="bg-image cefit-background-img"style=
-{{backgroundImage: `url('https://img.goodfon.com/original/1920x1080/e/97/shtanga-skamya-gym-fitness.jpg')`,
-height: "100%",
-width: "100%"}}>
-<div className="div-principal d-flex align-items-center justify-content-center mb-4   ">
-<div className="w-50 rounded-5 p-4 login-container">
-<CefetImage/>
-<form className="formulario-cadastro">
-<InputComponent classes="mb-3 mt-3" id="nameImput" text="Nome Completo" type="text" placeholder="Seu nome completo aqui"/>
-<InputComponent classes="mb-3" id="InputEmail" text="Email" type="email" placeholder="Insira seu email aqui"/>
-<InputComponent classes="mb-3" id="CPFInput" text="CPF" type="text" placeholder="Seu CPF aqui"/>
-<InputComponent classes="mb-3" id="age" text="Data de Nascimento" type="date" placeholder=""/>
-<InputComponent classes="mb-3" id="Password" text="Senha" type="password" placeholder="Insira sua senha aqui"/>
-<SubmitButton nomeButton ="Cadastrar"/>
-<div class="cadastro-texto mt-3">
-Possui conta?<a href="./../tela-login/Login.jsx"> Faça o seu Login!</a>
-</div>
-</form>
-</div>
-</div>
-</div> */}
 export default Cadastro;
-// function hashCrypt(senha){
-//     return createHash('sha256').update(senha).digest("hex");
-// }
-
-// class Usuario{
-//     constructor(nome, senha){
-//         this.nome = nome;
-//         this.hash = hashCrypt(senha);
-//     }
-//     autentica(nome, senha){
-//         if(nome === this.nome && this.hash === hashCrypt(senha)){
-//             console.log("usuario autenticado com sucesso");
-//             return true;
-//         }
-//         console.log("nome ou senha errados");
-//         return false;
-//     }
-// }
-
-// const usuario = new Usuario("caio", "haha");
-
-// console.log(usuario.autentica("Caio","haha"));
