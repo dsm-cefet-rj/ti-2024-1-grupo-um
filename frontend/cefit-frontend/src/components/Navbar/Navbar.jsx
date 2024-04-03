5b51f4482cc576f0faac5cfd12071046f63093e9
Vinicius Drumond <98275866+niviciuzdrm@users.noreply.github.com>
2024-04-03 14:14:15 -0300

    Adjust navbar css

--- frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
+++ frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
@@ -5,40 +5,42 @@ import { Link } from "react-router-dom";
 function Navbar(){
     return(
         <>
-        <nav className="navbar navbar-expand-lg fixed-top">
-            <div className="container-fluid">
-                {/* logo */}
-                <a className="navbar-brand me-auto" href="#"><img src={logo} alt="logo" className="logo-img"/></a>
+            <div className="nav-container">
+                <nav className="navbar navbar-expand-lg fixed-top">
+                    <div className="container-fluid">
+                        {/* logo */}
+                        <a className="navbar-brand me-auto" href="#"><img src={logo} alt="logo" className="logo-img"/></a>
 
-                <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" className="logo-img"/></h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                            <div className="offcanvas-header">
+                                <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" className="logo-img"/></h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div className="offcanvas-body">
+                                <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
+                                    <li className="nav-item">
+                                        <Link className="nav-link mx-lg-2" aria-current="page" to={"/"}>Home</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link mx-lg-2" aria-current="page" to={"/areaFIT"}>Área FIT</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link mx-lg-2" aria-current="page" to={"/personais"}>Personais</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="d-flex">
+                            <Link to="/login" className="signup-button">Sign in</Link>
+                            <Link to="/cadastro" className="signup-button">Sign up</Link>
+                        </div>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
                     </div>
-                    <div className="offcanvas-body">
-                        <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
-                            <li className="nav-item">
-                                <Link className="nav-link mx-lg-2" aria-current="page" to={"/"}>Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                 <Link className="nav-link mx-lg-2" aria-current="page" to={"/areaFIT"}>Área FIT</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link mx-lg-2" aria-current="page" to={"/personais"}>Personais</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="d-flex">
-                    <Link to="/login" className="signup-button">Sign in</Link>
-                    <Link to="/cadastro" className="signup-button">Sign up</Link>
-                </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                </nav>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
             </div>
-        </nav>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         </>
         
     )
