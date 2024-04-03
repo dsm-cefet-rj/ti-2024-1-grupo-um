466e187b104c19ca3311f92f9404da77960b2d93
Vinicius Drumond <98275866+niviciuzdrm@users.noreply.github.com>
2024-04-03 20:37:16 -0300

    Update FormList Component

--- frontend/cefit-frontend/src/components/FormList/formList.jsx
+++ frontend/cefit-frontend/src/components/FormList/formList.jsx
@@ -5,14 +5,14 @@ function FormList({items, listTitle, buttonText, buttonAction }) {
     return(
         <>
             <div className="my-4">
-                <span className="d-flex justify-content-between mb-4">
-                    <h3 className="listTitle">{listTitle}</h3>
-                    <button className="btn btn-primary" onClick={buttonAction}>{buttonText}</button>
+                <span className="d-flex justify-content-between mb-4 list-header">
+                    <h3 className="list-title">{listTitle}</h3>
+                    <button className="btn btn-primary list-button" onClick={buttonAction}>{buttonText}</button>
                 </span>
                 <div className="list-group cefit-list">
                     {items.map((item) => {
                         return (
-                            <Link to="#" className="list-group-item list-group-item-action">
+                            <Link to="#" className="list-group-item list-group-item-action border-custom" key={items.findIndex((element) => element === item)}>
                                 <div className="d-flex w-100 flex-column">
                                     <h5 className="mb-1">{item.name}</h5>
                                     <small>{item.description}</small>
