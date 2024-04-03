ecd0cb8cc1c585af94254be50f2e3297b0761252
Vinicius Drumond <98275866+niviciuzdrm@users.noreply.github.com>
2024-04-03 20:38:18 -0300

    Fix Navbar errors

--- frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
+++ frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar(){
                         {/* logo */}
                         <a className="navbar-brand me-auto" href="#"><img src={logo} alt="logo" className="logo-img"/></a>
 
-                        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                             <div className="offcanvas-header">
                                 <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" className="logo-img"/></h5>
                                 <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -34,12 +34,11 @@ function Navbar(){
                             <Link to="/login" className="signup-button">Sign in</Link>
                             <Link to="/cadastro" className="signup-button">Sign up</Link>
                         </div>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
-                            <span class="navbar-toggler-icon"></span>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
                     </div>
                 </nav>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
             </div>
         </>
         
