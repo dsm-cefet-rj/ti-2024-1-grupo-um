23b8acfc502df7cc1d3699b2e33e09d73127ba0c
accoelhos <anacarolina.sacoelho@gmail.com>
2024-04-13 00:01:08 -0300

    fix toggler and btn close offcanvas

--- frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
+++ frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar(){
                         <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                             <div className="offcanvas-header">
                                 <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" className="logo-img"/></h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
                             <div className="offcanvas-body">
                                 <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
@@ -59,7 +59,7 @@ function Navbar(){
                                 
                         }
                        
-                        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <button className="navbar-toggler ml-auto custom-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                     </div>
