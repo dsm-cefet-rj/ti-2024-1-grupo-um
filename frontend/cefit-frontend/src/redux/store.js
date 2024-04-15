cd96295e88643e91e322ec6318fd5f214b5c76e4
Vinicius Drumond <98275866+niviciuzdrm@users.noreply.github.com>
2024-04-15 18:39:41 -0300

    Implement AsyncThunk

--- frontend/cefit-frontend/src/redux/store.js
+++ frontend/cefit-frontend/src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import logger from "redux-logger"
+import { thunk } from "redux-thunk";
 
 import rootReducer from "./root-reducer"
 
 const store = configureStore({
     reducer: rootReducer,
-    middleware: () => {return [logger]}
+    middleware: () => {return [logger, thunk]}
 });
 
 export default store;
