55c952e9ba4055a3ea1182883807585db4141d95
Vinicius Drumond <98275866+niviciuzdrm@users.noreply.github.com>
2024-04-19 22:14:44 -0300

    add Anamenese on Navbar

--- frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
+++ frontend/cefit-frontend/src/components/Navbar/Navbar.jsx
@@ -7,27 +7,27 @@ import { logoutUser } from "../../redux/user/slice";
 
 
 
-function Navbar(){
-    const user =  useSelector(rootReducer => rootReducer.user);
+function Navbar() {
+    const user = useSelector(rootReducer => rootReducer.user);
     const dispatch = useDispatch();
-    const currentUserNavbar  = user.logged ? user.user : null;
+    const currentUserNavbar = user.logged ? user.user : null;
     // if(currentUserNavbar != null && currentUserNavbar != undefined){
     //     var id = currentUserNavbar.id;
     // }
-    function Logout(){
+    function Logout() {
         dispatch(logoutUser());
     }
-    return(
+    return (
         <>
             <div className="nav-container">
                 <nav className="navbar navbar-expand-lg fixed-top" id="navbar2">
                     <div className="container-fluid">
                         {/* logo */}
-                        <a className="navbar-brand me-auto" href="/#"><img src={logo} alt="logo" className="logo-img"/></a>
+                        <a className="navbar-brand me-auto" href="/#"><img src={logo} alt="logo" className="logo-img" /></a>
 
                         <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                             <div className="offcanvas-header">
-                                <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" className="logo-img"/></h5>
+                                <h5 className="offcanvas-title" id="offcanvasNavbarLabel"><img src={logo} alt="logo" className="logo-img" /></h5>
                                 <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
                             <div className="offcanvas-body">
@@ -41,24 +41,29 @@ function Navbar(){
                                     <li className="nav-item">
                                         <Link className="nav-link mx-lg-2" aria-current="page" to={"/personais"}>Personais</Link>
                                     </li>
+                                    {currentUserNavbar && (
+                                        <li>
+                                            <Link className="nav-link mx-lg-2" aria-current="page" to={"/anamnese"}>Anamnese</Link>
+                                        </li>
+                                    )}
                                 </ul>
                             </div>
                         </div>
                         {
-                                currentUserNavbar ? (
-                                    <div>
-                                        <a>Olá, {currentUserNavbar.nome} </a>
-                                        <button to="/login" className="signup-button" type="button" onClick={Logout}>logout</button>
-                                    </div>
-                                ) : (
-                                    <div className="d-flex">
-                                        <Link to="/login" className="signup-button">Sign in</Link>
-                                        <Link to="/cadastro" className="signup-button">Sign up</Link>
-                                    </div>
-                                )
-                                
+                            currentUserNavbar ? (
+                                <div>
+                                    <a>Olá, {currentUserNavbar.nome} </a>
+                                    <button to="/login" className="signup-button" type="button" onClick={Logout}>logout</button>
+                                </div>
+                            ) : (
+                                <div className="d-flex">
+                                    <Link to="/login" className="signup-button">Sign in</Link>
+                                    <Link to="/cadastro" className="signup-button">Sign up</Link>
+                                </div>
+                            )
+
                         }
-                       
+
                         <button className="navbar-toggler ml-auto custom-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -66,7 +71,7 @@ function Navbar(){
                 </nav>
             </div>
         </>
-        
+
     )
 }
 
