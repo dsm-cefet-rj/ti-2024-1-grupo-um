785e0e486fa1e69b4540d1a38636aea563b19722
luizhgdantas <luizdantas3105@gmail.com>
2024-04-22 14:37:13 -0300

    update cefetimage

--- frontend/cefit-frontend/src/components/CefetImage/CefetImage.jsx
+++ frontend/cefit-frontend/src/components/CefetImage/CefetImage.jsx
@@ -4,7 +4,9 @@ import "./CefetImage.css";
 function CefetImage(color){
     return(
         <div className="cefit-logo text-center rounded-5 m-auto">
+            <a href="/#">
                 <img src={logo} alt="foto cefit" className="p-1" width="100%" height="100%"/>
+            </a>
         </div>
     );
 }
