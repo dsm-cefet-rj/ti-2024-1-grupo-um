aaf2cff2faa025eff7f5e0e82e09d34c5785f228
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-04-22 18:55:54 -0300

    fix layout tela pagamento

--- frontend/src/pages/tela-pagamento/Pagamento.jsx
+++ frontend/src/pages/tela-pagamento/Pagamento.jsx
@@ -10,7 +10,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createAluno } from '../../redux/aluno/slice';
 //images
 import User from '../../images/user.png';
-
+//css
+import './Pagamento.css';
 
 function Pagamento() {
   const currentUser = useSelector(rootReducer => rootReducer.user);
@@ -51,37 +52,36 @@ function Pagamento() {
     <div>
       <Navbar/>
       <div className="bg-image cefit-background-img" style={{backgroundImage: `url('https://usercontent.one/wp/ignitetraininghub.se/wp-content/uploads/2022/09/25102022-_MS_6087-HDR-scaled.jpg')`, display: 'flex', flexDirection: 'column', gap: '20px' }}>
-        <div className="login-container d-flex justify-content-center align-items-center rounded-5 p-3">
+        <div className="pag-container">
         {personalAtual.image ?
               (
-                <div className="d-flex justify-content-center">
+                <div className="infos d-flex flex-column justify-content-center">
                   <img src={require(`../../images/PersonalImages/${personalAtual.image}.png`)} alt="avatar"
-                    className="img-fluid rounded-5" style={{ width: "150px" }} />
+                    className="img-personal" style={{ width: "100px" }} />
                   <h5 className="my-3 d-flex justify-content-center align-items-center">{personalAtual.nome}</h5> 
-                  <h1>{personalAtual.preco}</h1>
+                  <h1 className="preco">R${personalAtual.preco}</h1>
                 </div>
               ):(
                 <div>
                   <img src={User} alt="caso2"
-                  className="rounded-circle img-fluid" style={{ width: "150px" }} />
+                  className="rounded-circle img-fluid" style={{ width: "100px" }} />
                   <h5 className="my-3">{personalAtual.nome}</h5> 
+                  <h1>{personalAtual.preco}</h1>
                 </div>
               )
             }
-        </div>
-
-        <div className="login-container rounded-5 p-3">
-          
           <form className="adicionar-cartao mb-3" onSubmit={handlePayment}>
             <InputComponent classes="mt-3" id="numeroInput" text="Número do Cartão" type="text" placeholder="XXXX XXXX XXXX XXXX" value={numero} onChange={(e) => setNumero(e.target.value)} />
             <InputComponent classes="mt-3" id="cvcInput" text="CVC" type="text" placeholder="***" value={cvc} onChange={(e) => setCvc(e.target.value)} />
             <InputComponent classes="mt-3" id="expiracaoInput" text="Data de Expiração" type="text" placeholder="MM/AA" value={expiracao} onChange={(e) => setExpiracao(e.target.value)} />
             <InputComponent classes="mt-3" id="nomeInput" text="Nome no Cartão" type="text" placeholder="Insira nome no cartão" value={nome} onChange={(e) => setNome(e.target.value)} />
-            <div className="d-flex justify-content-center">
-              <button className="btn btn-primary mt-2" type='Submit'>Confirmar compra</button>
-            </div>
+            
           </form>
+          <div className="d-flex justify-content-center">
+              <button className="btn-submit" type='Submit'>Confirmar compra</button>
+            </div>
         </div>
+
       </div>
 
       <FooterComp/>
