9dc04144eb0f4a7191fd05b395841847987abb7f
Vinicius Drumond <98275866+niviciuzdrm@users.noreply.github.com>
2024-04-23 20:50:50 -0300

    Update PersonalCard.jsx

--- frontend/src/components/PersonalCard/PersonalCard.jsx
+++ frontend/src/components/PersonalCard/PersonalCard.jsx
@@ -1,6 +1,6 @@
 // import imagens
-import star  from "../../images/star.png";
-import user  from "../../images/user.png";
+import star from "../../images/star.png";
+import user from "../../images/user.png";
 
 // import CSS
 import "./PersonalCard.css";
@@ -8,11 +8,11 @@ import "./PersonalCard.css";
 // react router dom
 import { Link } from "react-router-dom";
 
-function PersonalCard({ nome, descricao, rating, id, image = user }){
+function PersonalCard({ nome, descricao, rating, id, image }) {
     return (
         <Link className="card" id="card-personal" to={`/personal/${id}`}>
-            {image ? (<img className="card-img-top m-auto mt-2" src={require(`../../images/PersonalImages/${image}.png`)} alt="Imagem de capa do card"/>)
-            :(<img className="card-img-top m-auto mt-2" src={user} alt="Imagem de capa do card"/>)}
+            {image ? (<img className="card-img-top m-auto mt-2" src={require(`../../images/PersonalImages/${image}.png`)} alt="Imagem de capa do card" />)
+                : (<img className="card-img-top m-auto mt-2" src={user} alt="Imagem de capa do card" />)}
             <div className="card-body text-center">
                 <h5 className="card-title">{nome}</h5>
                 <p className="card-text">{descricao}</p>
