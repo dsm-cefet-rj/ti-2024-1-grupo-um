89e0aad4058abad0341fc25e35cb317fafae2492
caiosantosdev <caiosantosdev@gmail.com>
2024-07-30 15:39:10 -0300

    feat: added login, fixed updateUser and deleteUser.
    Co-authored-by: Luiz Dantas <luizhgdantas@users.noreply.github.com>

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -42,64 +42,114 @@ async function createUser(req, res){
         //desmembrar objeto
         const newUser = {
             nome: req.body.nome,
-            cpf: req.body.CPF,
+            CPF: req.body.CPF,
             email: req.body.email,
             birth: req.body.birth,
             senha: req.body.senha
         }
         
-        console.log(req.body);
+        // console.log(req.body);
 
         //verificar email
         const existingEmail = await userModel.find({email: req.body.email});
-        if(existingEmail){
+
+        if(existingEmail.length > 0){
             return res.status(400).send({
                 message: 'Email ja cadastrado'
             });
         }
 
         //verificar cpf
-        const existingCPF = await userModel.find({CPF: req.body.cpf});
-        if(existingCPF){
-            return res.satus(400).send({
+        const existingCPF = await userModel.find({CPF: newUser.CPF});
+
+        if(existingCPF.length > 0){
+            return res.status(400).send({
                 message: 'Cpf já cadastrado'
             });
         }
 
+        //hash de senha
+        console.log(newUser);
         const result = await userModel.create(newUser);
 
         return res.status(201).send({
             message: 'Usuario criado com sucesso',
             usuario: newUser
-        })
+        });
 
     }catch(error){
         return res.status(400).send({
             error: error,
             message: 'Ocorreu um erro na criacao de usuario'
-        })
+        });
+    }
+}
+async function updateUser(req, res) {
+    try{
+        const id = req.params.id;
+
+        const update = req.body;
+
+        const updatedUser = await userModel.findByIdAndUpdate(id, update);
+
+        return res.status(200).send({
+            message:"usuario atualizado com sucesso.",
+            data: update
+        });
+
+    }catch(error){
+        return res.status(400).send({
+            message:"ocorreu um erro na tentativa da atualização",
+            erro: error
+        });
     }
 }
 async function deleteUser(req, res){
     try{
-        const userDelete = await userModel.findById(req.params.id);
-        if(userDelete){
-            const result = await userDelete.remove();
-            res.status(200).send({
-                message: 'Usuário removido com sucesso'
-            })
-        }
+        const id = req.params.id;
+        const userDelete = await userModel.findByIdAndDelete(id);
+        
+        return res.status(200).send({
+            message: 'Usuário removido com sucesso',
+            data: userDelete
+        });
+       
     }catch(error){
-        return res.status(400).sen({
+        return res.status(400).send({
             message: 'Ocorreu um erro para remover o usuário'
         })
     }
 }
+
 async function login(req, res){
     try{
+        const email = req.body.email;
+        const senha = req.body.senha;
 
+        const existingUserArray = await userModel.find({email});
+        const existingUser = existingUserArray[0];
+        if(existingUser == undefined){
+            return res.status(400).send({
+                message: "Login ou Senha errados."
+            });
+        }
+        //hash de senha
+        if(senha === existingUser.senha){
+            console.log(req.body);
+            return res.status(200).send({
+                message: "usuario autenticado com sucesso",
+                token: "hahahatoken"
+            });
+        }else{
+            return res.status(400).send({
+                message: "Login ou Senha errados.",
+            })
+        }
     }catch(error){
-
+        return res.status(400).send({
+            message: "ocorreu um erro no login",
+            erro: error
+        });
     }
 }
 async function logout(req, res){
@@ -112,4 +162,4 @@ async function logout(req, res){
 
 
 
-export { readAll, createUser, deleteUser, readOne };
+export { readAll, readOne, createUser, updateUser, deleteUser, login };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { readAll, createUser } from "../controllers/userController.js";
+import { readAll, createUser, readOne, updateUser, deleteUser, login } from "../controllers/userController.js";
 
 const userRoutes = express.Router();
 
+userRoutes.get('/user/:id', readOne);
 userRoutes.get('/user', readAll);
 userRoutes.post('/user', createUser);
+userRoutes.put('/user/:id', updateUser);
+userRoutes.delete('/user/:id', deleteUser);
+
+userRoutes.post('/login', login);
 
 export default userRoutes;
