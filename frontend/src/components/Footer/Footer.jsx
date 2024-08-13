c1eb6c57bf9dc3b99125339b0f96557464e51fac
caiosantosdev <caiosantosdev@gmail.com>
2024-08-13 15:16:55 -0300

    fix: fixed areaFit Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,32 +4,34 @@ import logo from "../../images/logo.png";
 import "./Footer.css"
 function FooterComp() {
   return (
-    <Footer container>
-      <div className="w-full text-center">
-        <div className="w-full justify-between sm:flex sm:justify-between"id="footer-infos">
-          <FooterBrand 
-            href="#"
-            src={logo}
-            alt="CEFIT Logo"
-            
-            className="logo"
-            
-          />
+    <div>
+      <Footer container>
+        <div className="w-full text-center">
+          <div className="w-full justify-between sm:flex sm:justify-between"id="footer-infos">
+            <FooterBrand 
+              href="#"
+              src={logo}
+              alt="CEFIT Logo"
+              
+              className="logo"
+              
+            />
+            <FooterLinkGroup>
+              <FooterLink  style={{ color: 'white' }} id="footer-link" href="#">Sobre</FooterLink>
+              <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/dsm-cefet-rj/ti-2024-1-grupo-um">Github</FooterLink>
+            </FooterLinkGroup>
+          </div>
+          <FooterDivider />
           <FooterLinkGroup>
-            <FooterLink  style={{ color: 'white' }} id="footer-link" href="#">Sobre</FooterLink>
-            <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/dsm-cefet-rj/ti-2024-1-grupo-um">Github</FooterLink>
+            <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/accoelhos">Ana Carolina</FooterLink>
+            <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/caiosantosdev">Caio Santos</FooterLink>
+            <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/luizhgdantas">Luiz Dantas</FooterLink>
+            <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/niviciuzdrm">Vinicius Drumond</FooterLink>
           </FooterLinkGroup>
+          <FooterCopyright style={{ color: 'white' }} href="#" by ="" year={2024} />
         </div>
-        <FooterDivider />
-        <FooterLinkGroup>
-          <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/accoelhos">Ana Carolina</FooterLink>
-          <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/caiosantosdev">Caio Santos</FooterLink>
-          <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/luizhgdantas">Luiz Dantas</FooterLink>
-          <FooterLink  style={{ color: 'white' }} id="footer-link" href="https://github.com/niviciuzdrm">Vinicius Drumond</FooterLink>
-        </FooterLinkGroup>
-        <FooterCopyright style={{ color: 'white' }} href="#" by ="" year={2024} />
-      </div>
-    </Footer>
+      </Footer>
+    </div>
   );
 }
 export default FooterComp
--- frontend/src/pages/tela-area-fit/AreaFIT.jsx
+++ frontend/src/pages/tela-area-fit/AreaFIT.jsx
@@ -16,57 +16,69 @@ import { v4 as idGen } from "uuid";
 //axios
 import { getAnamnese } from "../../redux/anamnese/slice";
 
-
 function AreaFIT() {
+	const dispatch = useDispatch();
+	const currentUser = useSelector((rootReducer) => rootReducer.user);
+	const trainings = useSelector((rootReducer) => rootReducer.trainings);
 
-  const dispatch = useDispatch();
-  const currentUser =  useSelector(rootReducer => rootReducer.user);
-  const trainings = useSelector(rootReducer => rootReducer.trainings);
-  
+	dispatch(clearExercises());
+	dispatch(getAnamnese(currentUser.user._id));
 
-  dispatch(clearExercises());
-  dispatch(getAnamnese(currentUser.user._id));
+	// Filtrar os treinos pelo idUser
+	if (!currentUser.logged) {
+		return <NotLoggedInAreaFIT />;
+	}
 
-  
-  // Filtrar os treinos pelo idUser
-  if (!currentUser.logged) {
-    return <NotLoggedInAreaFIT />;
-  }
+	return (
+		<>
+    <div style={{ height: "100vh" }}>
 
-  return (
-    <>
-      <Navbar />
-      <div className="fit-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 className="display-4">Área FIT</h1>
-        <p className="lead">Monte seus treinos ideais ou visualize seus treinos montados pelo personal</p>
-      </div>
-    <div className="container mt-2" id="container-card">
-        <div className="row justify-content-center"id="row-card">
-        {currentUser.logged && (
-            trainings.map((treino, index) => (
-              <div key={index} className="col mb-3">
-                <TreinoCard
-                  title={treino.title}
-                  description={treino.descricao}
-                  type={treino.type}
-                  id={treino._id} //talvez mudar para _id
-          
-                />
-              </div>)
-              )) 
-          }
-        </div>
+			<div style={{height:"80%"}}>
+				<Navbar />
+				<div className="fit-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+					<h1 className="display-4">Área FIT</h1>
+					<p className="lead">
+						Monte seus treinos ideais ou visualize seus treinos
+						montados pelo personal
+					</p>
+				</div>
+				<div className="container mt-2" id="container-card">
+					<div className="row justify-content-center" id="row-card">
+						{currentUser.logged &&
+							trainings.map((treino, index) => (
+								<div key={index} className="col mb-3">
+									<TreinoCard
+										title={treino.title}
+										description={treino.descricao}
+										type={treino.type}
+										id={treino._id} //talvez mudar para _id
+									/>
+								</div>
+							))}
+					</div>
+				</div>
+
+				<Link to={"/add-treinos"} className="link-monte-treino">
+					<button
+						className="monte-button"
+						onClick={() => {
+							dispatch(
+								addForms({
+									userId: currentUser.user._id,
+									infos: {},
+								})
+							);
+						}}
+					>
+						Monte seu treino
+					</button>
+				</Link>
+				<div className="espacamento"></div>
+			</div>
+			<FooterComp />
     </div>
-
-    <Link to={"/add-treinos"} className="link-monte-treino" >
-      <button className="monte-button" onClick={() => {dispatch(addForms({userId: currentUser.user._id, infos: {}}))}}>Monte seu treino</button>
-    </Link>
-    <div className="espacamento">
-
-      </div>
-    <FooterComp />
-    </>
-  );
+		</>
+	);
 }
 
 export default AreaFIT;
