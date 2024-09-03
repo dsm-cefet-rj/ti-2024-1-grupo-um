242cedfb109ddffc24a40c0a8029b905933960f6
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-09-02 22:13:05 -0300

    feat: add verificaçao de tipo imagem e CPF

--- frontend/src/components/InputComponent/InputComponenteYup.jsx
+++ frontend/src/components/InputComponent/InputComponenteYup.jsx
@@ -10,30 +10,37 @@ function InputComponentYup(props) {
             {props.type === "file" ? (
                 <Field name={props.name}>
                     {({ field, form }) => (
-                        <input
-                            type="file"
-                            id={props.id}
-                            className="form-control bg-light input-cefit w-100"
-                            onChange={(event) => {
-                                form.setFieldValue(props.name, event.currentTarget.files[0]);
-                            }}
-                        />
+                        <>
+                            <input
+                                type="file"
+                                id={props.id}
+                                className="form-control bg-light input-cefit w-100"
+                                accept=".jpeg, .jpg, .png" // Aceitar apenas arquivos JPEG e PNG
+                                onChange={(event) => {
+                                    const file = event.currentTarget.files[0];
+                                    form.setFieldValue(props.name, file);
+                                }}
+                            />
+                            <ErrorMessage name={props.name} render={renderError} />
+                        </>
                     )}
                 </Field>
             ) : (
-                <Field
-                    name={props.name}
-                    type={props.type}
-                    id={props.id}
-                    className="form-control bg-light input-cefit w-100"
-                    placeholder={props.placeholder}
-                    maxLength={props.type === "text" && props.name === "CPF" ? "11" : undefined} // Limita o CPF a 11 caracteres
-                    inputMode={props.name === "CPF" ? "numeric" : undefined} // Teclado numérico para CPF
-                    pattern={props.name === "CPF" ? "\\d*" : undefined} // Aceita apenas números para CPF
-                    onInput={props.name === "CPF" ? (e) => e.target.value = e.target.value.replace(/\D/g, '') : undefined} // Apenas números
-                />
+                <>
+                    <Field
+                        name={props.name}
+                        type={props.type}
+                        id={props.id}
+                        className="form-control bg-light input-cefit w-100"
+                        placeholder={props.placeholder}
+                        maxLength={props.type === "text" && props.name === "CPF" ? "11" : undefined}
+                        inputMode={props.name === "CPF" ? "numeric" : undefined}
+                        pattern={props.name === "CPF" ? "\\d*" : undefined}
+                        onInput={props.name === "CPF" ? (e) => e.target.value = e.target.value.replace(/\D/g, '') : undefined}
+                    />
+                    <ErrorMessage name={props.name} render={renderError} />
+                </>
             )}
-            <ErrorMessage name={props.name} render={renderError} />
         </div>
     );
 }
